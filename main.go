b22741fa28e5bdf1ad501243831ffb972e662b1e
Paul Reiter <reiter.paul@gmail.com>
2025-05-25 11:25:21 +0200

    Add minmal serving and requesting code

--- main.go
+++ main.go
@@ -2,13 +2,37 @@ package main
 
 import (
 	"fmt"
+	"io"
+	"log"
+	"net/http"
 	"os"
 )
+
+func queryHandler(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintln(w, "secret")
+}
+
+func queryTarget(target string) string {
+	resp, err := http.Get(target + ":1234/accio")
+	if err != nil {
+		log.Fatalf("Request failed: %v", err)
+	}
+	defer resp.Body.Close()
+
+	body, err := io.ReadAll(resp.Body)
+	if err != nil {
+		log.Fatalf("Reading response failed: %v", err)
+	}
+	return string(body)
+}
 
 func main() {
 	if len(os.Args) > 1 {
 		fmt.Println("Query target:", os.Args[1])
+		result := queryTarget(os.Args[1])
+		fmt.Println(result)
 	} else {
-		fmt.Println("No target provided, starting server")
+		http.HandleFunc("/accio", queryHandler)
+		log.Fatal(http.ListenAndServe(":1234", nil))
 	}
 }
